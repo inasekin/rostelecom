377d897b1726470110cb105881eb3184da59b4e3
Ivan Nasekin <nasekin@ipavlov.ai>
2022-03-12 01:37:58 +0300

    finish desktop version hero block

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
+import {fullPage} from "./modules/fullPage.js";
 
+fullPage();
 flsFunctions.isWebp();
-
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
