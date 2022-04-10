d4819cd8ff0de18327230b7eb8240d77ddbfe330
semyonnasekin <s.nasekin28@gmail.com>
2022-04-10 20:31:50 +0300

    added window text;

--- src/js/modules/tooltips.js
+++ src/js/modules/tooltips.js
@@ -1,6 +1,14 @@
 import { PointCircle } from "../data.js";
 import {createPopper} from "@popperjs/core";
 
+[...Array(4)].forEach((_, id) => {
+    const close = document.querySelector(`#tooltip-close-main-${id + 1}`);
+    const tooltip = document.querySelector(`#tooltip-main-${id + 1}`);
+    close.addEventListener("click", () => {
+        tooltip.setAttribute('tooltip-hide', "");
+    })
+})
+
 PointCircle.forEach((frame, idx) => {
     const index = idx + 1;
     const header = document.querySelector(`#tooltip-header-${index}`);
