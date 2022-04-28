196d4a5d3939f31aa740c123a907f32c136f1f8e
rodionveretin <rodion.veretin@gmail.com>
2022-04-28 16:19:37 +0300

    Правки 1

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -16,5 +16,9 @@ const swiper = new Swiper('.swiper', {
         el: ".swiper-pagination",
         clickable: true,
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
